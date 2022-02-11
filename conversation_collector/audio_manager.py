c962453a347688f32a83d5eb79b4d88e5b9b6219
chengfeng_luo <chengfeng_luo@apple.com>
2022-02-10 17:55:03 -0800

    finish up audio manager design

--- conversation_collector/audio_manager.py
+++ conversation_collector/audio_manager.py
@@ -10,13 +10,15 @@ fs = 44100
 sample_width = 2 # 2 bytes per sample
 channel_num = 1 # single channel
 
-# Wavefile strcture
-#  - path: string, file path
-#  - status: file status
-#       0: closed
-#       1: read
-#       2: write 
 class my_wavefile:
+    """
+    Wavefile strcture
+     - path: string, file path
+     - status: file status
+      0: closed
+      1: read
+      2: write 
+    """
     def __init__(self, path, status):
         self.path = path
         self.status = status
@@ -26,12 +28,18 @@ wf_closed = 0
 wf_read   = 1
 wf_write  = 2
 
-# AudioManager manage all the audio files status:
-#  - offer index to all available files
-#  - track what file is open for read or write (can't be both)
-#  - offer wave file handle by index
-#  - create new wave file，name by timestamp
 class AudioManager:
+    """
+    AudioManager manage all the audio files status:
+     - offer index to all available files
+     - track what file is open for read or write (can't be both)
+     - offer wave file handle by index
+     - create new wave file，name by timestamp
+
+    Drawback of current implementation:
+     - don't support delet (by the structure)
+     - don't track who is having the handle (can add this feature in future)
+    """
 
     def __init__(self):
         self.truth = toolings.get_truth()
@@ -46,11 +54,11 @@ class AudioManager:
             for f in fs:
                 self.file_list.append(my_wavefile(os.path.join(root,f), wf_closed))
 
-    """ create a new wavefile to write
+    def new(self):
+        """ create a new wavefile to write
         input : none
         return: index
-    """
-    def new(self):
+        """
         now_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         file_path = os.path.join(folder_path, "%s.wav"%now_time)
         wf = wave.open(file_path,'w')
@@ -64,11 +72,11 @@ class AudioManager:
         self.wf_w_handle_dict[index] = wf
         return index
         
-    """ Close a opened wavefile (also for saving files)
+    def close(self, index):
+        """ Close a opened wavefile (also for saving files)
         input : index
         return: 0:done -1:file is closed
-    """
-    def close(self, index):
+        """
         # check if in write dict
         if self.file_list[index].status==wf_write:
             wf = self.wf_w_handle_dict.pop(index)
@@ -82,21 +90,45 @@ class AudioManager:
             return -1
         return 0
 
-    """ Open a wave file for reading
-        input : index
-        return: index:success -1:failed
-    """
     def read(self, index):
-        # TODO
-        return "Decide what file format we use"
+        """ Open a wave file for reading
+        input : index
+        return: index:(success) -1(failed)
+        """
+        wf = wave.open(self.file_list[index].path)
+        ## TODO: add execption handling code
+        self.file_list[index].status = wf_read
+        self.wf_r_handle_dict[index] = wf
+        return index
 
-    """ Get wavefile number (also index range)
-    """
     def len(self):
+        """ Get number of wavefiles (also index range)
+        """
         return len(self.file_list)
 
-    """ Print all wavefile lists
-    """
-    def list(self):
-        # TODO
-        return ["1.wav", "2.mp3"]
+    def get_read_handle(self, index):
+        """ get wavefile read handle
+        input : index
+        return: read handle(success) -1(failed)
+        """
+        if self.file_list[index].status == wf_read:
+            return self.wf_r_handle_dict[index]
+        else:
+            return -1 
+
+    def get_write_handle(self, index):
+        """ get wavefile write handle
+        input : index
+        return: write handle(success) -1(failed)
+        """
+        if self.file_list[index].status == wf_write:
+            return self.wf_w_handle_dict[index]
+        else:
+            return -1 
+
+    def print_list(self):
+        """ print wavefile lists
+        """
+        for my_file in self.file_list:
+            print(my_file.name)
+        return
